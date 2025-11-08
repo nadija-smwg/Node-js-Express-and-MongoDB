469f79e9cd48939be410e0227194f2f07c8ff9e8
Nadijasmwg <nadijasmwg@gmail.com>
2025-11-08 12:37:56 +0530

    11/8

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const http = require('http');
+
+
+//Blocking , synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn);
+// const textOut = `this is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File Written!')
+
+//Non - Blocking , asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('Error! 💥💥💥');
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2)
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3)
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', err => {
+                console.log("your file is been written 😁")
+
+            })
+
+        });
+    });
+});
+console.log('will read file')
+
